29eff1016d42b9d68146b4943d7e3d60f9cf97a3
Andrew Anca <97320300+AndrewA1537@users.noreply.github.com>
2023-10-08 14:12:05 -0700

    Removed the cors packages.

--- server/app.js
+++ server/app.js
@@ -1,150 +1,151 @@
 /**
  * RESOURCES USED:
- * 
+ *
  * In class lecture based code examples
- * 
+ *
  * used for css styling and simple html
  * https://www.w3schools.com/css/
- * 
+ *
  * used for commenting, and code generation
  * https://chat.openai.com/
  */
 
-
 // Import required modules
 const HTTP = require("http");
-const URL  = require("url");
-const CORS = require("cors");
-
+const URL = require("url");
+// const CORS = require("cors");
 
 // Define constants for the service's endpoint and default port
 const SERVICE_ROOT_ENDPOINT = "/api/definitions/";
 const PORT = process.env.PORT || 10000;
 const POST = "POST";
-const GET  = "GET";
-
+const GET = "GET";
 
 // Array to store definitions in memory
 let definitions = [];
 
-
 // Create the server
-const server = HTTP.createServer((req, res) => 
-{
+const server = HTTP.createServer((req, res) => {
+    // Set CORS headers to allow any origin
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    // Handle CORS preflight OPTIONS requests
+    if (req.method === "OPTIONS") {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
     // Use CORS middleware to handle CORS headers
-    CORS()(req, res, () => 
-    {
-        // Handle POST requests to add new definitions
-        if(req.method === POST && req.url === SERVICE_ROOT_ENDPOINT) 
-        {
-            let body = "";
+    //   CORS()(req, res, () => {
+    // Handle POST requests to add new definitions
+    if (req.method === POST && req.url === SERVICE_ROOT_ENDPOINT) {
+        let body = "";
 
-            // Concatenate incoming chunks of data to form the complete request body
-            req.on("data", (chunk) => 
-            {
-                body += chunk.toString();
-            });
+        // Concatenate incoming chunks of data to form the complete request body
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
 
-            req.on("end", () => 
-            {
-                try 
-                {
-                    const data = JSON.parse(body);
+        req.on("end", () => {
+            try {
+                const data = JSON.parse(body);
 
-                    // Check if the word already exists in the definitions array
-                    const existingDefinition = definitions.find(
-                        (def) => def.word === data.word
-                    );
+                // Check if the word already exists in the definitions array
+                const existingDefinition = definitions.find(
+                    (def) => def.word === data.word
+                );
 
-                    if(existingDefinition) 
-                    {
-                        // Send a response indicating that the word already exists
-                        res.writeHead(409, 
-                        {
-                            "Content-Type": "application/json",
-                        });
-                        res.end(
-                            JSON.stringify({
-                                success: false,
-                                message: `Warning! '${data.word}' already exists.`,
-                            })
-                        );
-                    } 
-                    else
-                    {
-                        // Add the received definition to the in-memory array
-                        definitions.push(data);
-
-                        // Send a success response
-                        res.writeHead(200, {
-                            "Content-Type": "application/json",
-                        });
-                        res.end(
-                            JSON.stringify({
-                                success: true,
-                                message: `Request #${definitions.length} (the number of total entries in your dictionary)\nNew entry recorded:\n"${data.word} : ${data.definition}"`,
-                            })
-                        );
-                    }
-                } 
-                catch(error)
-                {
-                    // Handle invalid JSON in request body
-                    res.writeHead(400, { "Content-Type": "application/json" });
+                if (existingDefinition) {
+                    // Send a response indicating that the word already exists
+                    res.writeHead(409, {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    });
                     res.end(
                         JSON.stringify({
                             success: false,
-                            message: "Invalid JSON data",
+                            message: `Warning! '${data.word}' already exists.`,
+                        })
+                    );
+                } else {
+                    // Add the received definition to the in-memory array
+                    definitions.push(data);
+
+                    // Send a success response
+                    res.writeHead(200, {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    });
+                    res.end(
+                        JSON.stringify({
+                            success: true,
+                            message: `Request #${definitions.length} (the number of total entries in your dictionary)\nNew entry recorded:\n"${data.word} : ${data.definition}"`,
                         })
                     );
                 }
-            });
-        }
-        // Handle GET requests to retrieve definitions
-        else if(req.method === GET && req.url.startsWith(SERVICE_ROOT_ENDPOINT))
-        {
-            const parsedURL  = URL.parse(req.url, true);
-            const searchTerm = parsedURL.query.word;
-
-            // Search for the definition in the in-memory array
-            const foundDefinition = definitions.find(
-                (def) => def.word === searchTerm
-            );
-
-            if(foundDefinition)
-            {
-                res.writeHead(200, { "Content-Type": "application/json" });
-                res.end(
-                    JSON.stringify({
-                        success: true,
-                        definition: foundDefinition.definition,
-                    })
-                );
-            } 
-            else
-            {
-                // Send a not found response if the word doesn't exist in the definitions array
-                res.writeHead(404, { "Content-Type": "application/json" });
+            } catch (error) {
+                // Handle invalid JSON in request body
+                res.writeHead(400, {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                });
                 res.end(
                     JSON.stringify({
                         success: false,
-                        message: "Word not found!",
+                        message: "Invalid JSON data",
                     })
                 );
             }
-        } 
-        else
-        {
-            // Handle all other routes/methods
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            res.end("Not Found");
-        }
-    });
-});
+        });
+    }
+    // Handle GET requests to retrieve definitions
+    else if (req.method === GET && req.url.startsWith(SERVICE_ROOT_ENDPOINT)) {
+        const parsedURL = URL.parse(req.url, true);
+        const searchTerm = parsedURL.query.word;
 
+        // Search for the definition in the in-memory array
+        const foundDefinition = definitions.find(
+            (def) => def.word === searchTerm
+        );
+
+        if (foundDefinition) {
+            res.writeHead(200, {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            res.end(
+                JSON.stringify({
+                    success: true,
+                    definition: foundDefinition.definition,
+                })
+            );
+        } else {
+            // Send a not found response if the word doesn't exist in the definitions array
+            res.writeHead(404, {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            });
+            res.end(
+                JSON.stringify({
+                    success: false,
+                    message: "Word not found!",
+                })
+            );
+        }
+    } else {
+        // Handle all other routes/methods
+        res.writeHead(404, {
+            "Content-Type": "text/plain",
+            "Access-Control-Allow-Origin": "*",
+        });
+        res.end("Not Found");
+    }
+});
+// });
 
 // Start the server
-server.listen(PORT, () => 
-{
+server.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
