b3edaf8b0df2bdeab4b1fc1f4efd1f164992e9f7
AndrewA1537 <andrewanca1998@hotmail.com>
2023-10-08 10:56:42 -0700

    Commit all changed files.

--- client/search.js
+++ client/search.js
@@ -1,38 +1,62 @@
-document.addEventListener("DOMContentLoaded", function () 
+// URL for the API endpoint
+const endPointRoot = "http://localhost:3000/api/definitions/";
+const GET          = "GET";
+
+
+// Wait until the DOM content has loaded before running the script
+document.addEventListener("DOMContentLoaded", function ()
 {
-    const form = document.getElementById("searchForm");
+    // Get references to the form and the div to display search results
+    const form            = document.getElementById("searchForm");
     const searchResultDiv = document.getElementById("searchResult");
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent the default form submission
+    // Add an event listener to the form to handle submissions
+    form.addEventListener("submit", function (event) 
+    {
+        // Prevent the form from its default submission behavior
+        event.preventDefault();
 
+        // Extract the search term from the input field
         const searchTerm = document.getElementById("searchTerm").value;
 
-        // Assuming your API uses query parameters to get search results
-        fetch(endPointRoot + "?word=" + searchTerm, {
+        // Send a GET request to the server to search for the term
+        fetch(endPointRoot + "?word=" + searchTerm, 
+        {
             method: GET,
-            headers: {
+            headers: 
+            {
                 "Content-Type": "application/json",
             },
         })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((jsonResponse) => {
-                if (jsonResponse.success) {
-                    searchResultDiv.textContent = jsonResponse.definition;
-                } else {
-                    searchResultDiv.textContent = "Word not found!";
-                }
-            })
-            .catch((error) => {
-                console.error(
-                    "There was an issue with the fetch operation:",
-                    error.message
-                );
-            });
+        .then((response) => 
+        {
+            // If the HTTP response isn't in the 200-299 range, reject the promise
+            if(!response.ok) 
+            {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((jsonResponse) => 
+        {
+            // Display the definition if found, otherwise display an error message
+            if(jsonResponse.success) 
+            {
+                searchResultDiv.textContent = jsonResponse.definition;
+            } 
+            else 
+            {
+                searchResultDiv.textContent = "Word not found!";
+            }
+        })
+        .catch((error) => 
+        {
+            // Handle any errors that occurred during the fetch
+            console.error
+            (
+                "There was an issue with the fetch operation:",
+                error.message
+            );
+        });
     });
 });
--- client/store.js
+++ client/store.js
@@ -1,51 +1,65 @@
+// Endpoint where the definitions will be sent
 const endPointRoot = "http://localhost:3000/api/definitions/";
+
+
+// Constants for the HTTP methods being used
 const POST = "POST";
 const GET = "GET";
 
 
+// Event listener to ensure the DOM content is loaded before our code runs
 document.addEventListener("DOMContentLoaded", function () 
 {
+    // Select the form element from the page
     const form = document.querySelector("form");
 
+    // Add an event listener to handle form submissions
     form.addEventListener("submit", function (event) 
     {
-        event.preventDefault(); // Prevent the default form submission
+        // Prevent the form from doing its default submit action (e.g., page refresh)
+        event.preventDefault();
 
+        // Get values from the input fields
         const word       = document.getElementById("word").value;
         const definition = document.getElementById("definition").value;
         const data       = { word: word, definition: definition };
 
-        fetch(endPointRoot, {
+        // Make a POST request to the server with the word and its definition
+        fetch(endPointRoot, 
+        {
             method: POST,
-            headers: {
+            headers: 
+            {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(data),
         })
-            .then((response) => 
+        .then((response) => 
+        {
+            // If the response is not OK (e.g., status 404 or 500), reject the promise
+            if(!response.ok) 
             {
-                if(!response.ok)
-                {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
-                return response.json();
-            })
-            .then((jsonResponse) => 
-            {
-                console.log(jsonResponse);
-                // Handle the response here
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(() => 
+        {
+            // Response received successfully
+            // TODO: Add any required logic here if needed.
 
-                // Clear input fields after successful submission
-                document.getElementById("word").value       = "";
-                document.getElementById("definition").value = "";
-            })
-            .catch((error) =>
-            {
-                // Handle the error here, both for failed network requests and when response.ok is false
-                console.error(
-                    "There was an issue with the fetch operation:",
-                    error.message
-                );
-            });
+            // Clear the input fields after successfully sending data
+            document.getElementById("word").value       = "";
+            document.getElementById("definition").value = "";
+        })
+        .catch((error) => 
+        {
+            // Handle any errors that occurred during the fetch
+            console.error
+            (
+                "There was an issue with the fetch operation:",
+                error.message
+            );
+        });
     });
 });
